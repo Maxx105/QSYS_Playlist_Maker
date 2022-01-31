6935652889a045cc8c976fc4e2540dc9df33f9aa
Maxx Sanner <maxxsanner105@gmail.com>
2022-01-31 10:08:49 -0500

    added graphics to layout.lua

--- layout.lua
+++ layout.lua
@@ -1,19 +1,59 @@
--- table.insert(graphics,{
---   Type = "GroupBox",
---   Text = "Control",
---   Fill = {200,200,200},
---   StrokeWidth = 1,
---   Position = {5,5},
---   Size = {200,100}
--- })
--- table.insert(graphics,{
---   Type = "Text",
---   Text = "Say Hello:",
---   Position = {10,42},
---   Size = {90,16},
---   FontSize = 14,
---   HTextAlign = "Right"
--- })
+table.insert(graphics,{
+  Type = "GroupBox",
+  Text = "Playlist Maker",
+  Fill = {200,200,200},
+  StrokeWidth = 1,
+  Position = {6,5},
+  Size = {303,576}
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "Audio Player:",
+  Position = {45,107},
+  Size = {72,16},
+  FontSize = 12,
+  HTextAlign = "Left"
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "Songs:",
+  Position = {45,123},
+  Size = {72,16},
+  FontSize = 12,
+  HTextAlign = "Left"
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "Directory:",
+  Position = {45,139},
+  Size = {72,16},
+  FontSize = 12,
+  HTextAlign = "Left"
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "File:",
+  Position = {45,155},
+  Size = {72,16},
+  FontSize = 12,
+  HTextAlign = "Left"
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "Name",
+  Position = {35,185},
+  Size = {167,18},
+  FontSize = 12,
+  HTextAlign = "Center"
+})
+table.insert(graphics,{
+  Type = "Text",
+  Text = "Time",
+  Position = {202,185},
+  Size = {75,16},
+  FontSize = 12,
+  HTextAlign = "Center"
+})
 -- layout["SendButton"] = {
 --   PrettyName = "Buttons~Send The Command",
 --   Style = "Button",
