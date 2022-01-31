b12c55f014693df38b06a2932a674ffb94bcbf34
Maxx Sanner <maxxsanner105@gmail.com>
2022-01-31 09:24:25 -0500

    initial commit, basic framework

---
+++ info.lua
@@ -0,0 +1,8 @@
+PluginInfo = {
+  Name = "Playlist Maker",
+  Version = "0.0",
+  BuildVersion = "0.0.0.0",
+  Id = "1e886705-4f8b-403e-9a2f-02bdb899a0db",
+  Author = "QSC",
+  Description = "A simple plugin that allows you to create a playlist out of a single audio file"  
+}
