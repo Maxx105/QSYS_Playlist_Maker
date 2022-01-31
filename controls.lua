0b363f4a536803de87ce9271a792b0ef01a425ce
Maxx Sanner <maxxsanner105@gmail.com>
2022-01-31 10:01:45 -0500

    added controls to controls.lua

--- controls.lua
+++ controls.lua
@@ -1,9 +1,89 @@
--- table.insert(ctrls, {
---   Name = "SendButton",
---   ControlType = "Button",
---   ButtonType = "Momentary",
---   Count = 1,
---   UserPin = true,
---   PinStyle = "Input",
---   Icon = "Power"
--- })
+table.insert(ctrls, {
+  Name = "NextSong",
+  ControlType = "Button",
+  ButtonType = "Trigger",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "PreviousSong",
+  ControlType = "Button",
+  ButtonType = "Trigger",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "SubmitSongInfo",
+  ControlType = "Button",
+  ButtonType = "Trigger",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "RemoveSong",
+  ControlType = "Button",
+  ButtonType = "Trigger",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "UpdateSong",
+  ControlType = "Button",
+  ButtonType = "Trigger",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "Songs",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "AddName",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "AddTime",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "AudioPlayerSelect",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "ListOfSongs",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "AllDirectories",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
+table.insert(ctrls, {
+  Name = "AllFiles",
+  ControlType = "Text",
+  UserPin = true,
+  PinStyle = "Both",
+  Count = 1
+})
